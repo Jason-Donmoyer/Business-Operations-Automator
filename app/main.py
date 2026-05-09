e00f214ee0748ee4010e29cb4ae6ba4ac416ba14
Jason Donmoyer <jasondonmoyer@Mac.lan>
2026-05-09 07:14:53 -0400

    scheduler.py complete

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.routes.upload import router as uploader
 from app.routes.reports import router as reporter
+from app.scheduler import scheduler
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.start()
+    yield
+    scheduler.stop()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(uploader)
 app.include_router(reporter)
+
 
 @app.get("/")
 def read_root():
---
+++ app/scheduler.py
@@ -0,0 +1,19 @@
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.cleaner import clean_csv
+from app.reporter import generate_report
+
+def process_uploads(uploads_dir='data/uploads'):
+    uploads = os.listdir(uploads_dir)
+    csv_files = []
+    for upload in uploads:
+        if upload.split(".")[-1] == "csv":
+            csv_files.append(upload)
+    for file in csv_files:
+        file_path = 'data/uploads/' + file
+        clean_data = clean_csv(file_path)
+        generate_report(clean_data, file)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(process_uploads, "interval", hours=1)
+
