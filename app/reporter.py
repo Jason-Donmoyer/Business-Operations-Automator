062edd29a9dc4f7c2e9199c993a6dfdbaa8ad6d4
Jason Donmoyer <jasondonmoyer@Mac.lan>
2026-05-10 07:47:28 -0400

    Fixed Issues: - fixed prodeuct count issue - added total sales tax to report - fixed cleaner to catch lower case product data

--- app/reporter.py
+++ app/reporter.py
@@ -7,10 +7,12 @@ def generate_report(data: pd.DataFrame, filename: str):
     path = 'data/reports/' + filename
     os.makedirs(path, exist_ok=True)
     total_revenue = int(data.revenue.sum())
+    total_sales_tax = int(data.sales_tax.sum())
     revenue_by_category = data.groupby('category').revenue.sum().to_dict()
-    product_volume = data.groupby('product').count().to_dict(orient="index")
+    product_volume = data.groupby('product')['units_sold'].sum().to_dict()
     content = {
         'total_revenue': total_revenue, 
+        'total_sales_tax': total_sales_tax,
         'revenue_by_category': revenue_by_category, 
         'product_volume': product_volume
     }
