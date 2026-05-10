19bd8e1153456f71a5aa650bc4594f7268e06dc8
Jason Donmoyer <jasondonmoyer@Jasons-MacBook-Air.local>
2026-05-10 05:31:49 -0400

    reporter now creates a json and an xlsx file saved in a subdirectory of data/reports

--- app/reporter.py
+++ app/reporter.py
@@ -1,9 +1,11 @@
+import os
 import pandas as pd
 import json
 
 def generate_report(data: pd.DataFrame, filename: str):
     filename = filename.split('.')[0]
-    path = 'data/reports/' + filename + '.json'
+    path = 'data/reports/' + filename
+    os.makedirs(path, exist_ok=True)
     total_revenue = int(data.revenue.sum())
     revenue_by_category = data.groupby('category').revenue.sum().to_dict()
     product_volume = data.groupby('product').count().to_dict(orient="index")
@@ -12,6 +14,7 @@ def generate_report(data: pd.DataFrame, filename: str):
         'revenue_by_category': revenue_by_category, 
         'product_volume': product_volume
     }
-    with open(path, "w") as f:
+    with open(path + '/' + filename + '.json', "w") as f:
         json.dump(content, f, indent=4)
+    data.to_excel(path + '/' + filename + '.xlsx', index=False, engine='openpyxl')
     return content
