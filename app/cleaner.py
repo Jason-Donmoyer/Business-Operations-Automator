fa7ee82c68936bb5878ffe9c5a6d4bb616133460
Jason Donmoyer <jasondonmoyer@Jasons-MacBook-Air.local>
2026-05-27 05:13:01 -0400

    Fixed bug in cleaner.py and reporter.py for null data handling

--- app/cleaner.py
+++ app/cleaner.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 def clean_csv(file_path: str):
     df = pd.read_csv(file_path).drop_duplicates()
-    df['category'] = df['category'].str.lower()
-    df['product'] = df['product'].str.title()
+    if 'category' in df.columns:
+        df['category'] = df['category'].str.lower()
+    if 'product' in df.columns:
+        df['product'] = df['product'].str.title()
     df[df.select_dtypes(include='str').columns] = df.select_dtypes(include='str').apply(lambda x: x.str.strip().str.split().str.join(' '))
     df[df.select_dtypes(include='number').columns] = df.select_dtypes(include='number').fillna(0)
     df[df.select_dtypes(include=['object', 'str']).columns] = df.select_dtypes(include=['object', 'str']).fillna("Unknown")
--- app/reporter.py
+++ app/reporter.py
@@ -6,10 +6,10 @@ def generate_report(data: pd.DataFrame, filename: str):
     filename = filename.split('.')[0]
     path = 'data/reports/' + filename
     os.makedirs(path, exist_ok=True)
-    total_revenue = int(data.revenue.sum())
-    total_sales_tax = int(data.sales_tax.sum())
-    revenue_by_category = data.groupby('category').revenue.sum().to_dict()
-    product_volume = data.groupby('product')['units_sold'].sum().to_dict()
+    total_revenue = int(data['revenue'].sum()) if 'revenue' in data.columns else 0
+    total_sales_tax = int(data['sales_tax'].sum()) if 'sales_tax' in data.columns else 0
+    revenue_by_category = data.groupby('category')['revenue'].sum().to_dict() if 'category' in data.columns and 'revenue' in data.columns else {}
+    product_volume = data.groupby('product')['units_sold'].sum().to_dict() if 'product' in data.columns and 'units_sold' in data.columns else {}
     content = {
         'total_revenue': total_revenue, 
         'total_sales_tax': total_sales_tax,
